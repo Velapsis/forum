8d54fc7a90f554c72edeb607c8e759d9248813c4
Velapsis <romain.Vicente@ynov.Com>
2025-03-20 13:00:07 +0100

    change import

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"forum/web"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "gdhs")
-		log.Print("bjr")
-	})
+	web.Init()
+	web.CreateWebsite()
+
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "gdhs")
+	// 	log.Print("bjr")
+	// })
 
 	fmt.Println("Serveur démarré sur le port 8080")
 	http.ListenAndServe(":8080", nil)
