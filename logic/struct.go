1201a9b700da5e722a3548c6fc765cc4e59c11c8
Ploum <keo.ployer@ynov.com>
2025-03-24 10:57:19 +0100

    function for session + change struct

--- logic/struct.go
+++ logic/struct.go
@@ -1,5 +1,7 @@
 package logic
-import "database/sql"
+import ("database/sql"
+		"time"
+		)
 
 var regex Regex
 var user User
@@ -17,6 +19,30 @@ type User struct {
 	Email    string
 	Password string
 	UUID     int
+	// Provider   string
+	// ProviderID string
+	// CreatedAt  time.Time
+}
+// type GoogleUserInfo struct {
+// 	ID            string `json:"id"`
+// 	Email         string `json:"email"`
+// 	VerifiedEmail bool   `json:"verified_email"`
+// 	Name          string `json:"name"`
+// 	Picture       string `json:"picture"`
+// }
+
+// type GithubUserInfo struct {
+// 	ID        int    `json:"id"`
+// 	Login     string `json:"login"`
+// 	Name      string `json:"name"`
+// 	Email     string `json:"email"`
+// 	AvatarURL string `json:"avatar_url"`
+// }
+type Session struct {
+	ID        string
+	UserID    int
+	ExpiresAt time.Time
+	CreatedAt time.Time
 }
 
 type SQL struct {
