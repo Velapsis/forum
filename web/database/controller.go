1d3737ee3955dac092a0e7b1693720b7582cfc76
Enzo <enzo.granana@ynov.com>
2025-03-24 10:44:19 +0100

    add user request + changes database logic

--- web/database/controller.go
+++ web/database/controller.go
@@ -1,19 +1,14 @@
 package database
 
 import (
-	"fmt"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 // User représente un utilisateur du forum
-func AddUser(request string, username string, email string, password string) {
-
-	// Execute database with provided request
-	output, err := database.Exec(request, username, email, password)
-	fmt.Println("Exec database output: ", output)
-	if err != nil {
-		fmt.Println("Error while attempting to add user ", username, " to database")
-		fmt.Println("Error: ", err)
-	}
+func AddUser(username string, email string, password string) {
+	args := []string{username, email, password}
+	token := strings.Join(args, ":")
+	Exec(token)
 }
