707369457100abdea1d9fdc8ee5465fa7925bc92
enzo <enzo.granana@ynov.com>
2025-04-02 19:39:53 +0200

    fix regex check js

--- web/static/script.js
+++ web/static/script.js
@@ -1,81 +1,59 @@
-// Booleans
-var isUsernameValid
-var isEmailValid
-var isPasswordValid
-
-// Input fields
-var Username = document.getElementById("username").value
-var Email = document.getElementById("email").value
-var Password = document.getElementById("password").value
-
-// Text hints
-var UsernameHint = document.getElementById("usernameHint").textContent
-var EmailHint = document.getElementById("emailHint").textContent
-var PasswordHint = document.getElementById("passwordHint").textContent
-
 // Loop
 setInterval(function() {CheckCredentials();}, 1000)
 
 function CheckCredentials() {
 
-    console.log("Checking..")
+    Username = document.getElementById("username").value
+    Email = document.getElementById("email").value
+    Password = document.getElementById("password").value
 
-    // Username check
-    if (!/^[A-Za-z0-9_]+$/.test(Username)) {
-        UsernameHint = "Invalid username: Only letters, numbers and underscores are allowed."
-        isUsernameValid = false
-    } else {
-        isUsernameValid = true
-    }
-    if (!Username.length > 4) {
-        UsernameHint = "Username is too short. Min length: 4"
-        console.log("Username is too short")
-        isUsernameValid = false
-    } else {
-        isUsernameValid = true
-    }
-    if (!Username.length < 16) {
-        UsernameHint = "Username is too long. Max length: 16"
-        isUsernameValid = true
-    } else {
-        isUsernameValid = false
-    }
-
-    // Email check
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(Email)) {
-        EmailHint = "Invalid email."
-        isEmailValid = false
-    } else {
-        isEmailValid = true
+    // USERNAME CHECK
+    // Check for invalid characters
+    if (Username != "") {
+        if (!/^[A-Za-z0-9_]+$/.test(Username)) {
+            document.getElementById("usernameHint").textContent = "Invalid username: Only letters, numbers and underscores are allowed."
+        }
+        // Check for length
+        else if (Username.length < 4) {
+            document.getElementById("usernameHint").textContent = "Username is too short. Min length: 4"
+        }
+        else if (Username.length > 16) {
+            document.getElementById("usernameHint").textContent = "Username is too long. Max length: 16"
+        }
+        // Valid username
+        else {
+            document.getElementById("usernameHint").textContent = ""
+        }
     }
 
-    // Password check
-    if (!Password.length < 8) {
-        PasswordHint = "Your password is too short. It should be at least 8 characters long."
-        isPasswordValid = false
-    } else {
-        isPasswordValid = true
-    }
-    if (!/^[1-9]+$/.test(Password)) {
-        PasswordHint = "YOur password should contain at least one number."
-        isPasswordValid = false
-    } else {
-        isPasswordValid = true
-    }
-    if (!/^[A-Z]+$/.test(Password)) {
-        PasswordHint = "Your password should contain at least one capital letter."
-        isPasswordValid = false
-    } else {
-        isPasswordValid = true
+    // EMAIL CHECK
+    if (Email != "") {
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(Email)) {
+            document.getElementById("emailHint").textContent = "Invalid email."
+        }
+        // Valid email
+        else {
+            document.getElementById("emailHint").textContent = ""
+        }
     }
 
-    //ResetHints()
-}
-
-function ResetHints() {
-    if (isUsernameValid && isEmailValid && isPasswordValid) {
-        UsernameHint = ""
-        EmailHint = ""
-        PasswordHint = ""
+    // PASSWORD CHECK
+    // Check for length
+    if (Password != "") {
+        if (Password.length < 8) {
+            document.getElementById("passwordHint").textContent = "Your password is too short. It should be at least 8 characters long."
+        }
+        // Check for number
+        else if (!/[1-9]/.test(Password)) {
+            document.getElementById("passwordHint").textContent = "Your password should contain at least one number."
+        }
+        // Check for caps letter
+        else if (!/[A-Z]/.test(Password)) {
+            document.getElementById("passwordHint").textContent = "Your password should contain at least one capital letter."
+        } 
+        // Valid password
+        else {
+            document.getElementById("passwordHint").textContent = ""
+        }
     }
 }
